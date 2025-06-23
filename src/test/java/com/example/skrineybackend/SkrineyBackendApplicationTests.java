0efe515ff74d31d7aefab74b108f4f64418df062
revus05 <skninni05@gmail.com>
2025-06-23 07:39:45 +0300

    initial commit

---
+++ src/test/java/com/example/skrineybackend/SkrineyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skrineybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkrineyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
