59fe0a672ccca4ee77530853dda14d835f3adcd0
revus05 <skninni05@gmail.com>
2025-08-03 19:54:27 +0300

    feat: make jwt token httpOnly

--- src/main/java/com/example/skrineybackend/service/CookieService.java
+++ src/main/java/com/example/skrineybackend/service/CookieService.java
@@ -9,6 +9,7 @@ public class CookieService {
     public void addJwtCookie(HttpServletResponse response, String token) {
         Cookie jwtCookie = new Cookie("jwt", token);
         jwtCookie.setSecure(true);
+        jwtCookie.setHttpOnly(true);
         jwtCookie.setPath("/");
         jwtCookie.setMaxAge(86400);
 
