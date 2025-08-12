f3ff142b6ab977645703f423d757b741ae0defec
revus05 <skninni05@gmail.com>
2025-08-12 12:27:01 +0300

    chore: apply env variables and make updates to docker-compose and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
+FROM maven:3.9.9-eclipse-temurin-17 AS build
+WORKDIR /workspace
+COPY pom.xml .
+RUN mvn -q -e -DskipTests dependency:go-offline
+COPY src ./src
+RUN mvn -q -DskipTests clean package
+
 FROM eclipse-temurin:17-jdk-alpine
 WORKDIR /app
-
-COPY target/skriney-backend-0.0.1-SNAPSHOT.jar app.jar
-
+COPY --from=build /workspace/target/skriney-backend-0.0.1-SNAPSHOT.jar app.jar
+# Если хотите, чтобы файл .env был в контейнере:
+COPY .env .env
 EXPOSE 8080
-
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/skrineybackend/SkrineyBackendApplication.java
+++ src/main/java/com/example/skrineybackend/SkrineyBackendApplication.java
@@ -1,6 +1,5 @@
 package com.example.skrineybackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,10 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SkrineyBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-		SpringApplication.run(SkrineyBackendApplication.class, args);
+        SpringApplication.run(SkrineyBackendApplication.class, args);
 	}
 }
