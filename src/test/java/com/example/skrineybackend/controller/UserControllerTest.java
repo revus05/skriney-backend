9dd54f3332d6cd464cd68c8e2c433effde925eeb
revus05 <skninni05@gmail.com>
2025-10-01 15:55:37 +0300

    test: add integrational tests for sign up endpoint

--- src/test/java/com/example/skrineybackend/controller/UserControllerTest.java
+++ src/test/java/com/example/skrineybackend/controller/UserControllerTest.java
@@ -1,55 +1,91 @@
 package com.example.skrineybackend.controller;
 
+import com.example.skrineybackend.AbstractIntegrationTest;
 import com.example.skrineybackend.dto.user.SignUpUserRequestDTO;
-import com.example.skrineybackend.dto.user.UserDTO;
-import com.example.skrineybackend.entity.User;
-import com.example.skrineybackend.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-public class UserControllerTest {
-    @Mock
-    private UserService userService;
-    @InjectMocks
-    private UserController userController;
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-    }
-
-
+@Transactional
+class UserControllerTest extends AbstractIntegrationTest {
     @Test
     void signUp_User_ValidRequest_ReturnsSuccessResponse() throws Exception {
         SignUpUserRequestDTO requestBody = new SignUpUserRequestDTO(
-            "testusername",
-            "testemail@gmail.com",
-            "testpassword"
+                "testusername",
+                "testemail@gmail.com",
+                "testpassword"
         );
 
-        String requestBodyJsonAuto = new ObjectMapper().writeValueAsString(requestBody);
-
-        UserDTO mockUser = new UserDTO(new User(requestBody));
-        when(userService.signUpUser(any(SignUpUserRequestDTO.class))).thenReturn(mockUser);
+        String requestBodyJson = new ObjectMapper().writeValueAsString(requestBody);
 
         mockMvc.perform(post("/users/sign-up")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(requestBodyJsonAuto))
-            .andExpect(status().isCreated());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.status").value(HttpStatus.CREATED.value()))
+                .andExpect(jsonPath("$.data.username").value("testusername"))
+                .andExpect(jsonPath("$.data.email").value("testemail@gmail.com"));
+    }
+
+    @Test
+    void signUp_User_UserWithUsernameExists_ReturnsErrorResponse() throws Exception {
+        SignUpUserRequestDTO requestBody1 = new SignUpUserRequestDTO(
+                "testusername",
+                "testemail@gmail.com",
+                "testpassword"
+        );SignUpUserRequestDTO requestBody2 = new SignUpUserRequestDTO(
+                "testusername",
+                "testemail123@gmail.com",
+                "testpassword"
+        );
+
+        String requestBodyJson1 = new ObjectMapper().writeValueAsString(requestBody1);
+        String requestBodyJson2 = new ObjectMapper().writeValueAsString(requestBody2);
+
+        mockMvc.perform(post("/users/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyJson1));
+
+        mockMvc.perform(post("/users/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBodyJson2))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
+                .andExpect(jsonPath("$.message").value("Запись с такими данными уже существует"))
+                .andExpect(jsonPath("$.data.username").value("Имя пользователя занято"));
+    }
+
+    @Test
+    void signUp_User_UserWithEmailExists_ReturnsErrorResponse() throws Exception {
+        SignUpUserRequestDTO requestBody1 = new SignUpUserRequestDTO(
+                "testusername",
+                "testemail@gmail.com",
+                "testpassword"
+        );SignUpUserRequestDTO requestBody2 = new SignUpUserRequestDTO(
+                "testusername123",
+                "testemail@gmail.com",
+                "testpassword"
+        );
+
+        String requestBodyJson1 = new ObjectMapper().writeValueAsString(requestBody1);
+        String requestBodyJson2 = new ObjectMapper().writeValueAsString(requestBody2);
+
+        mockMvc.perform(post("/users/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyJson1));
+
+        mockMvc.perform(post("/users/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBodyJson2))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
+                .andExpect(jsonPath("$.message").value("Запись с такими данными уже существует"))
+                .andExpect(jsonPath("$.data.email").value("Пользователь с таким email уже существует"));
     }
 }
