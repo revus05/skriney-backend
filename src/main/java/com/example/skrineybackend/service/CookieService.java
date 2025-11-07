8a35b9d251ee8b92a05ccf825d87c44cd8d9fcd8
revus05 <skninni05@gmail.com>
2025-11-07 17:21:53 +0300

    fix: update jwt cookie generation

--- src/main/java/com/example/skrineybackend/service/CookieService.java
+++ src/main/java/com/example/skrineybackend/service/CookieService.java
@@ -9,8 +9,10 @@ public class CookieService {
     public void addJwtCookie(HttpServletResponse response, String token) {
         Cookie jwtCookie = new Cookie("jwt", token);
         jwtCookie.setSecure(true);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setAttribute("SameSite", "None");
         jwtCookie.setPath("/");
-        jwtCookie.setMaxAge(86400);
+        jwtCookie.setMaxAge(86400 * 7);
 
         response.addCookie(jwtCookie);
     }
