007e3c92dc55a5e5fe1f2f50eb6ebe1c77dc7b7c
revus05 <skninni05@gmail.com>
2025-11-07 17:48:54 +0300

    fix: update jwt cookie generation

--- src/main/java/com/example/skrineybackend/service/CookieService.java
+++ src/main/java/com/example/skrineybackend/service/CookieService.java
@@ -1,19 +1,16 @@
 package com.example.skrineybackend.service;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CookieService {
     public void addJwtCookie(HttpServletResponse response, String token) {
-        Cookie jwtCookie = new Cookie("jwt", token);
-        jwtCookie.setSecure(true);
-        jwtCookie.setHttpOnly(true);
-        jwtCookie.setAttribute("SameSite", "None");
-        jwtCookie.setPath("/");
-        jwtCookie.setMaxAge(86400 * 7);
-
-        response.addCookie(jwtCookie);
+        String cookie = String.format(
+                "jwt=%s; Path=/; Max-Age=%d; Secure; HttpOnly; SameSite=None",
+                token,
+                86400 * 7
+        );
+        response.addHeader("Set-Cookie", cookie);
     }
 }
